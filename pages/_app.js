8c9b1a162d4f34dc7edda9cae1fb9eb4862a69ae
siripods <siripods@gmail.com>
2023-07-22 11:45:26 +0700

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css';
+import Layout from '../components/layout/layout';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name="description" content="Next JS Events"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp
